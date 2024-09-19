02c9390435c200d0bb3ef937c1636d5148eb097b
20IanHarris24 <ian_harris@ymail.com>
2024-09-19 11:03:02 +0200

    Logout btn has logic to only show when logged in

--- src/components/LogoutBtn.tsx
+++ src/components/LogoutBtn.tsx
@@ -1,18 +1,20 @@
 import { ReactElement } from "react";
 import { useAuthContext } from "../hooks";
+import { LoginPage } from "../pages";
 
-export function LoginStatusChip(): ReactElement {
+export function LogoutBtn(): ReactElement {
   const { isLoggedIn, logout } = useAuthContext();
 
   return (
     <div id="login-status-chip">
+         
       {isLoggedIn ? 
       (<div className="tooltip">
-         <button className="logout-btn" onClick={logout}>
+        <button className="logout-btn" onClick={logout}>
           <span className="material-symbols-outlined">logout</span>
           <span className="tooltiptext">Click to Log-out</span>
         </button>
-      </div>) : <></>}
+      </div>) : (<LoginPage/>)}
     </div>
   );
 }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,12 +1,12 @@
 import { ReactElement } from "react";
 import { Outlet } from "react-router-dom";
-import { LoginStatusChip } from "../components/LogoutBtn";
+import { LogoutBtn } from "../components/LogoutBtn";
 
 export function LandingPage(): ReactElement {
   return (
     <main id="landing-page" className="g-container">
       <h1 className="header">User Role Page</h1>
-      <LoginStatusChip />
+      <LogoutBtn />
       <Outlet />
     </main>
   );
