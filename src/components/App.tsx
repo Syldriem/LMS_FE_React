58ddbacbcdf3313c8b43aee675eee2d5bdf37933
milanesiluca <luca.milanesi@live.se>
2024-09-19 13:54:28 +0200

    test

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,6 +4,7 @@ import { router } from "../router";
 // import { LoginStatusChip } from "./LoginStatusChip";
 
 export function App() {
+  console.log("this is new compiknent");
   return (
     <AuthProvider>
       <RouterProvider router={router} />
