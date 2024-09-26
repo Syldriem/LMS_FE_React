3ae0f4aa24c39517689f2d108183fe67c5cc7a26
Panas425 <anasabualsoud@hotmail.com>
2024-09-26 02:19:17 +0200

    Update LogoutBtn.tsx

--- src/components/LogoutBtn.tsx
+++ src/components/LogoutBtn.tsx
@@ -4,26 +4,18 @@ import { LoginPage } from "../pages";
 import { useNavigate } from "react-router-dom";
 
 export function LogoutBtn(): ReactElement {
-  const { isLoggedIn, logout } = useAuthContext();
+  const { logout } = useAuthContext();
 
-  const navigate = useNavigate();
 
-  useEffect(() => {
-    if (isLoggedIn===true) {
-      navigate("/mycoursepage"); 
-    } else {
-      navigate("/login"); 
-    }
-  }, [isLoggedIn, navigate]);
   
   return (
     <div id="login-status-chip">
-      (<div className="tooltip">
+      <div className="tooltip">
         <button className="logout-btn" onClick={logout}>
           <span className="material-symbols-outlined">logout</span>
           <span className="tooltiptext">Click to Log-out</span>
         </button>
-      </div>)
+      </div>
     </div>
   );
 }
